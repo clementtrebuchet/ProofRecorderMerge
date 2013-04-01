1ecfd7d1e836f4c16621a591f90274018d516b11
namgyal <n.brisson@app-concept-lab.fr>
2013-04-01 02:26:18 +0200

    Data Layer implemented into Models

--- src/org/proof/recorder/database/models/Contact.java
+++ src/org/proof/recorder/database/models/Contact.java
@@ -2,22 +2,112 @@ package org.proof.recorder.database.models;
 
 import java.io.Serializable;
 
+import org.proof.recorder.database.support.ProofDataBase;
+import org.proof.recorder.personnal.provider.PersonnalProofContentProvider;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.telephony.PhoneNumberUtils;
+import android.util.Log;
 
 @SuppressWarnings("serial")
-public class Contact implements Serializable {
+public class Contact implements Serializable, DataLayerInterface {
 
 	private static final String DEFAULT_VALUE = "null";
+	
+	private static Uri mUri = Uri.withAppendedPath(
+			PersonnalProofContentProvider.CONTENT_URI,
+			"excluded_contacts");
+	
+	private static Uri mUriByPhone = Uri.withAppendedPath(
+			PersonnalProofContentProvider.CONTENT_URI, 
+			"excluded_contact_phone/");
 
 	private String id;
 	private String contactName;
 	private String phoneNumber;
 	private String contractId;
+	
+	private SimplePhoneNumber sPhoneNumber;
+	
+	public SimplePhoneNumber getsPhoneNumber() {
+		return sPhoneNumber;
+	}
 
+	private static ContentResolver resolver;
+	
+	private static boolean hasDataHandler;
+	
+	/**
+	 * @return
+	 */
+	public boolean isExcluded() {
+		
+		int count;
+		
+		Uri uri = Uri.withAppendedPath(
+				mUriByPhone, 
+				this.getsPhoneNumber().get_nationalNumber());
+		
+		Cursor cursor = getResolver().query(uri,
+				null, null, null, null);
+		
+		count = cursor.getCount();
+		
+		cursor.close();
+		
+		return count > 0;
+	}
+	
+	@Override
+	public void fillValues() {
+		DataLayerInterface._values.put(
+				ProofDataBase.COLUMN_CONTRACT_CONTACTS_ID, this.getContractId());
+		DataLayerInterface._values.put(
+				ProofDataBase.COLUMN_DISPLAY_NAME, this.getContactName());
+		DataLayerInterface._values.put(
+				ProofDataBase.COLUMN_PHONE_NUMBER, this.sPhoneNumber.get_nationalNumber());	
+	}
+
+	@Override
+	public void save() {
+		if(hasDataHandler()) {
+			this.fillValues();
+			getResolver().insert(mUri, DataLayerInterface._values);
+			int lastId = PersonnalProofContentProvider.lastInsertId(
+					"excludedcontactsproof");
+			this.setId("" + lastId);
+		}
+		else {
+			print_exception("No ContentResolver has been set via Contact" +
+					" Class method Could not save Contact Instance: " + this);
+		}
+	}
+
+	@Override
+	public String toString(){
+		String contactStr = "Id: " + this.getId() + 
+				" ContractId: " + this.getContractId() + 
+				" ContactName: " + this.getContactName() + 
+				" PhoneNumber: " + this.getContactName() + 
+				" PhoneNumber (Object): " + this.getsPhoneNumber();
+		return contactStr;
+	}
+
+	/**
+	 * 
+	 */
 	public Contact(){
 		this.defaultInit();
 	}
 
+	/**
+	 * @param id
+	 * @param apiId
+	 * @param name
+	 * @param phone
+	 */
 	public Contact(String id, 
 			String apiId, 
 			String name, 
@@ -25,10 +115,19 @@ public class Contact implements Serializable {
 		this.fullInit(id, apiId, name, phone);
 	}
 
+	/**
+	 * 
+	 */
 	private void defaultInit(){
 		this.fullInit(null, null, null, null);
 	}
 
+	/**
+	 * @param id
+	 * @param apiId
+	 * @param name
+	 * @param phone
+	 */
 	private void fullInit(
 			String id, 
 			String apiId, 
@@ -41,32 +140,64 @@ public class Contact implements Serializable {
 				apiId != null ? apiId : DEFAULT_VALUE);
 		this.setContactName(
 				name != null ? name : DEFAULT_VALUE);
-		this.setPhoneNumber(
-				phone != null ? phone : DEFAULT_VALUE);
+		this.phoneNumber =
+				phone != null ? phone : DEFAULT_VALUE;
+		
+		setHasDataHandler(false);
+		
+		if(phone != null)
+			this.sPhoneNumber = new SimplePhoneNumber(phone);
+		else
+			this.sPhoneNumber = new SimplePhoneNumber();
 	}
 
+	/**
+	 * @return
+	 */
 	public String getContactName() {
 		return contactName;
 	}
+	/**
+	 * @param contactName
+	 */
 	public void setContactName(String contactName) {
 		this.contactName = contactName;
 	}
+	/**
+	 * @return
+	 */
 	public String getId() {
 		return id;
 	}
+	/**
+	 * @param id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
+	/**
+	 * @return
+	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+	/**
+	 * @param phoneNumber
+	 */
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = PhoneNumberUtils.stripSeparators(phoneNumber);
+		this.sPhoneNumber.set_originalNumber(phoneNumber);
 	}
+	/**
+	 * @return
+	 */
 	public String getContractId() {
 		return contractId;
 	}
 
+	/**
+	 * @return
+	 */
 	public long getLongContractId() {
 		long id;
 		try {
@@ -77,16 +208,53 @@ public class Contact implements Serializable {
 		return id;
 	}
 
+	/**
+	 * @param contractId
+	 */
 	public void setContractId(String contractId) {
 		this.contractId = contractId;
 	}
+	
+	/**
+	 * @param message
+	 */
+	private void print(String message) {
+		Log.d(this.getClass().getName(), message);
+	}
+	
+	/**
+	 * @param message
+	 */
+	private void print_exception(String message) {
+		Log.e(this.getClass().getName(), message);
+	}		
 
-	@Override
-	public String toString(){
-		String contactStr = "Id: " + this.getId() + 
-				" ContractId: " + this.getContractId() + 
-				" ContactName: " + this.getContactName() + 
-				" PhoneNumber: " + this.getPhoneNumber();
-		return contactStr;
+	/**
+	 * @return the hasDataHandler
+	 */
+	public static boolean hasDataHandler() {
+		return hasDataHandler;
+	}
+
+	/**
+	 * @param hasDataHandler the hasDataHandler to set
+	 */
+	public static void setHasDataHandler(boolean hasDataHandler) {
+		Contact.hasDataHandler = hasDataHandler;
+	}
+	
+	/**
+	 * @return
+	 */
+	public static ContentResolver getResolver() {
+		return resolver;
+	}
+
+	/**
+	 * @param resolver
+	 */
+	public static void setResolver(ContentResolver resolver) {
+		Contact.resolver = resolver;
+		setHasDataHandler(true);
 	}
 }
--- src/org/proof/recorder/database/models/Record.java
+++ src/org/proof/recorder/database/models/Record.java
@@ -1,26 +1,177 @@
 package org.proof.recorder.database.models;
 
-public class Record {
-	
+import java.util.ArrayList;
+import java.util.List;
+
+import org.proof.recorder.database.support.ProofDataBase;
+import org.proof.recorder.personnal.provider.PersonnalProofContentProvider;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+public class Record implements DataLayerInterface {
+
+	// Uris 
+
+	private static Uri mUriRecords = Uri.withAppendedPath(
+			PersonnalProofContentProvider.CONTENT_URI, "records");
+
+	private static Uri mUriRecordById = Uri.withAppendedPath(
+			PersonnalProofContentProvider.CONTENT_URI, "record_by_id/");
+
+	// Private Attributes
+
 	private String mId;
 	private String mFilePath;
-	private String mPhone;
-	private String mHtime;	
+	private String mPhone;	
+	private String mHtime;
+	private String mTimeStamp;
 	private String mSense;
 	private String mAndroidId;
-	
-	public Record () {
-		setmId("-1");
-		setmFilePath("null");
+	private String mSize;	
+
+	private DataPhoneNumber mDataNumber;
+	private static ContentResolver mResolver;
+	private Contact mContact;
+
+	private boolean hasDataLayer = false;
+
+	// Logging wrapping
+
+	protected void print(String message) {
+		Log.d(this.getClass().getName(), message);
 	}
-	
+
+	protected void print_exception(String message) {
+		Log.e(this.getClass().getName(), message);
+	}
+
+	public void toConsole() {
+		this.print(this.toString());
+	}
+
+	@Override
+	public String toString() {
+		return "Record [mId=" + mId + ", mFilePath=" + mFilePath
+				+ ", mPhone=" + mPhone + ", mHtime=" + mHtime
+				+ ", mTimeStamp=" + mTimeStamp + ", mSense=" + mSense
+				+ ", mAndroidId=" + mAndroidId + ", mSize=" + mSize
+				+ ", mDataNumber=" + mDataNumber + ", mContact=" + mContact
+				+ ", hasDataLayer=" + hasDataLayer + "]";
+	}
+
+	public static List<Record> getAllObjects() {
+
+		List<Record> mRecordCollection =  new ArrayList<Record>();
+		Record mRecord;
+
+		Cursor mCursor = Record.getmResolver().query(
+				mUriRecords, null, null, null, null);
+
+		if(mCursor.moveToFirst()) {
+			do {
+				mRecord = new Record();
+				mRecordCollection.add(mRecord);
+			}while(mCursor.moveToNext());
+		}
+
+		return mRecordCollection;		
+	}
+
+	public static Record getRecordById(String id) {
+
+		Record mRecord = null;
+
+		Uri mUri = Uri.withAppendedPath(mUriRecordById, id);
+
+		Cursor mCursor = Record.getmResolver().query(
+				mUri, null, null, null, null);
+
+		if(mCursor.moveToFirst()) {
+			do {
+				mRecord = new Record();			
+			}while(mCursor.moveToNext());
+		}
+
+		return mRecord;		
+	}
+
+	public static List<Record> getRecordsByPhone(String phone) {
+
+		return null;		
+	}
+
+	public String getmTimeStamp() {
+		return mTimeStamp;
+	}
+
+	public void setmTimeStamp(String mTimeStamp) {
+		this.mTimeStamp = mTimeStamp;
+	}
+
+	public String getmSize() {
+		return mSize;
+	}
+
+	public void setmSize(String mSize) {
+		this.mSize = mSize;
+	}
+
+	public static Uri getmUriRecords() {
+		return mUriRecords;
+	}
+
+	public boolean isHasDataLayer() {
+		return hasDataLayer;
+	}
+
+	public static void setmUriRecords(Uri mUriRecords) {
+		Record.mUriRecords = mUriRecords;
+	}
+
+	public void setHasDataLayer(boolean hasDataLayer) {
+		this.hasDataLayer = hasDataLayer;
+	}
+
+	public Record () {
+		this.initialize("-1", "null");
+	}
+
 	public Record (String id, String fileName) {
+		this.initialize(id, fileName);
+	}
+
+	public Record (ContentResolver resolver, String id, String fileName) {
+		this.initialize(id, fileName);
+		this.mDataNumber = new DataPhoneNumber(resolver);
+	}
+
+	private void initialize(String id, String fileName) {
+		this.setmContact(new Contact());
 		setmId(id);
 		setmFilePath(fileName);
 	}
-	
+
 	public String getmHtime() {
 		return mHtime;
+	}
+
+	public DataPhoneNumber getmDataNumber() {
+		return mDataNumber;
+	}
+
+	public static ContentResolver getmResolver() {
+		return mResolver;
+	}
+
+	public void setmDataNumber(DataPhoneNumber mDataNumber) {
+		this.mDataNumber = mDataNumber;
+	}
+
+	public static void setmResolver(ContentResolver mResolver) {
+		Record.mResolver = mResolver;
 	}
 
 	public void setmHtime(String mHtime) {
@@ -73,7 +224,8 @@ public class Record {
 	/**
 	 * @param mPhone the mPhone to set
 	 */
-	public void setmPhone(String mPhone) {
+	public void setmPhone(String mPhone) {		
+		this.mDataNumber.set_originalNumber(mPhone);
 		this.mPhone = mPhone;
 	}
 
@@ -90,4 +242,36 @@ public class Record {
 	public void setmAndroidId(String mAndroidId) {
 		this.mAndroidId = mAndroidId;
 	}
+
+	public Contact getmContact() {
+		return mContact;
+	}
+
+	public void setmContact(Contact mContact) {
+		this.mContact = mContact;
+	}
+
+	private String getPhoneNumberFK() {
+		return this.mDataNumber.get_countryCode() + ";" + 
+				this.mDataNumber.get_nationalNumber();
+	}
+
+	@Override
+	public void fillValues() {
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_CONTRACT_ID, this.getmAndroidId());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_FILE, this.getmFilePath());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_TIMESTAMP, this.getmTimeStamp());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_HTIME, this.getmHtime());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_TELEPHONE, this.getPhoneNumberFK());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_SENS, this.getmSense());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_TAILLE, this.getmSize());
+		DataLayerInterface._values.put(ProofDataBase.COLUMN_ISYNC_PH, "0");		
+	}
+
+	@Override
+	public void save() {		
+		this.mDataNumber.save();
+		this.fillValues();
+		getmResolver().insert(mUriRecords, DataLayerInterface._values);	
+	}	
 }
