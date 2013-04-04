add2598941834ab23d078ade786eb0af53d05eff
namgyal <n.brisson@app-concept-lab.fr>
2013-04-04 22:33:11 +0200

    About dialog complete

--- src/org/proof/recorder/fragment/dialog/AboutApps.java
+++ src/org/proof/recorder/fragment/dialog/AboutApps.java
@@ -16,6 +16,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
@@ -24,10 +25,9 @@ public class AboutApps extends SherlockFragmentActivity {
 	private static Context mContext;
 	
 	private ListView mListView;
-	private Spanned[] mLinks = {			
-			Html.fromHtml("<a mailto:contact@frugandfrog.com >contact</a>"),
-			Html.fromHtml("<a href='www.frugandfrog.com' >www.frugandfrog.com</a>")
-	};
+	private TextView mAboutApp;
+	
+	private String[] mLinks;
 	
 	/**
 	 * @return the mContext
@@ -66,10 +66,18 @@ public class AboutApps extends SherlockFragmentActivity {
 		
 		setContext(this);
 		
+		mAboutApp = (TextView) findViewById(R.id.txt_app);
+		mAboutApp.setText(Html.fromHtml((String) getText(R.string.about_app)));
+		
+		mLinks = new String[]{			
+				(String) getText(R.string.contact_uri),
+				(String) getText(R.string.site_uri)
+		};
+		
 		mListView = (ListView) findViewById(R.id.some_links_list);
 		
 		mListView.setAdapter(
-				new ArrayAdapter<Spanned>(
+				new ArrayAdapter<String>(
 						this, 
 						android.R.layout.simple_list_item_1, 
 						mLinks)
@@ -81,9 +89,10 @@ public class AboutApps extends SherlockFragmentActivity {
 
 			@Override
 			public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
-				Spanned link = (Spanned)mListView.getItemAtPosition(position);
-				String mLink = cleanString(link.toString());
-				String frug = "frugandfrog.com";
+				String link = (String)mListView.getItemAtPosition(position);
+				String mLink = cleanString(link);
+				String phoneandvoice = "phoneandvoice-recorder.com";
+				String webMatch = "phone&voicerecorder";
 				
 				print("mLink:" + mLink);
 				
@@ -91,7 +100,7 @@ public class AboutApps extends SherlockFragmentActivity {
 					
 					print("mLink clicked!");
 					
-					String email = mLink + "@" + frug;
+					String email = mLink + "@" + phoneandvoice;
 					String subject = getContext().getString(R.string.contact_subject);
 					String body = getContext().getString(R.string.contact_body);					
 					
@@ -106,9 +115,12 @@ public class AboutApps extends SherlockFragmentActivity {
 					intent.setData(data);
 					startActivity(intent);
 				}
-				if(mLink.equals("www." + frug)) {
+				
+				if(mLink.equals(webMatch)) {
+					
 					print("mLink clicked!");
-					Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://" + mLink));
+					
+					Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www." + phoneandvoice));
 					startActivity(browserIntent);
 				}
 			}
