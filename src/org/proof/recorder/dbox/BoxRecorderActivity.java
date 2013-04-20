9daf196146803338019c49ec048509526cb4505a
namgyal <n.brisson@app-concept-lab.fr>
2013-04-20 14:23:05 +0200

    Some more dirs and extensions added to .gitignore

--- src/org/proof/recorder/dbox/BoxRecorderActivity.java
+++
@@ -1,9 +0,0 @@
-package org.proof.recorder.dbox;
-
-import com.actionbarsherlock.app.SherlockActivity;
-
-public class BoxRecorderActivity extends SherlockActivity{
-	private final String CALL_DIR = "/calls/";
-	private final String VOX_DIR = "/voices/";
-
-}
