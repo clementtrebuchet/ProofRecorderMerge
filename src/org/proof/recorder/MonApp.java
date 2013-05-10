a895d9b96fdb74b1c703dabd386dd499a7b35673
namgyal <n.brisson@app-concept-lab.fr>
2013-05-10 14:13:01 +0200

    ACRA try / catch hope will trap all strange crashes

--- src/org/proof/recorder/MonApp.java
+++ src/org/proof/recorder/MonApp.java
@@ -1,9 +1,8 @@
 package org.proof.recorder;
+
 import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
-
-
-
+import org.proof.recorder.utils.Log.Console;
 
 @ReportsCrashes(formKey = "dEdhREpjQThYVXNVakltRmhFSGktRmc6MQ") 
 public class MonApp extends android.app.Application {
@@ -11,7 +10,15 @@ public class MonApp extends android.app.Application {
 	@Override
     public void onCreate() {
         // The following line triggers the initialization of ACRA
-        ACRA.init(this);
+		
+		// Try / catch: might fix crashes on some devices.
+		try {
+			ACRA.init(this);
+		}
+		catch(Exception e) {
+			Console.print_exception(e);
+		}
+        
         super.onCreate();
     }
 }
