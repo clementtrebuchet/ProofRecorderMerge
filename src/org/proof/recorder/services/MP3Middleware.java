f5e7b92765b77ce69474a31f0b78c5346acbde14
clement <clement.trebuchet@gmail.com>
2013-05-15 15:52:45 +0200

    ++add aidl for ogg and finish all

--- src/org/proof/recorder/services/MP3Middleware.java
+++ src/org/proof/recorder/services/MP3Middleware.java
@@ -1,5 +1,6 @@
 package org.proof.recorder.services;
 
+import org.proof.recorder.utils.PlugMiddleware;
 import org.proof.recorder.utils.Log.Console;
 import org.proofs.recorder.codec.mp3.utils.IServiceIntentRecorderMP3;
 import org.proofs.recorder.codec.mp3.utils.IServiceIntentRecorderMP3Cx;
@@ -8,7 +9,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class MP3Middleware extends Service  {
+public class MP3Middleware extends Service implements PlugMiddleware  {
 
 	
 	//private IServiceIntentRecorderMP3Cx connection = null;
@@ -20,6 +21,8 @@ public class MP3Middleware extends Service  {
 	private int outBitrate;
 	private int postEncode;
 	private String broadcastClass;
+	private String notificationIntent;
+	private String notificationPkg;
 	
 	 @Override
 	  public void onCreate() {
@@ -33,8 +36,10 @@ public class MP3Middleware extends Service  {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
+		
 				this.remotePlugCnx = new IServiceIntentRecorderMP3Cx(this);
 				this.remotePlugCnx.safelyConnectTheService();
+				
 				while(this.remotePlugCnx == null){
 					Console.print_debug("this.remotePlugCnx is :"+this.remotePlugCnx);
 				}
@@ -48,29 +53,49 @@ public class MP3Middleware extends Service  {
 				if (broadcastClass == null){
 					broadcastClass = "org.proofs.recorder.codec.mp3.MainActivity";
 				}
+				if (intent.getExtras().containsKey("notificationIntent")) {
+					
+					notificationIntent = intent
+							.getStringExtra("notificationIntent");
+				} else {
+					
+					notificationIntent = "org.proofs.recorder.codec.mp3.MainActivity";
+					
+				}
+				
+				if (intent.getExtras().containsKey("notificationPkg")) {
+					
+					notificationPkg = intent
+							.getStringExtra("notificationPkg");
+					
+				} else {
+					
+					notificationPkg = "org.proofs.recorder.codec.mp3";
+				}
 				Console.print_debug(mFile+"/"+mSampleRate+"/"+audioSource+"/"+outBitrate+"/"+broadcastClass);			
 		return(START_STICKY);	
 	}
-		
+	
+	
+	@Override
 	 public void parametersRecAsynchronously(int message) {
-		 Console.print_debug("proof parametersRecAsynchronously: "+message);
+		 Console.print_debug("proof parametersRecAsynchronously RC =: "+message);
 	 
 	 }
-	 
+	@Override
 	 public void startRecAsynchronously(int message){
-		 Console.print_debug("proof startRecAsynchronously: "+message);
+		 Console.print_debug("proof startRecAsynchronously RC =: "+message);
 	 }
-	 
+	@Override
 	 public void stopRecAsynchronously(int message){
-		 Console.print_debug("proof stopRecAsynchronously: "+message);
+		 Console.print_debug("proof stopRecAsynchronously RC =: "+message);
 	 }
-
+	@Override
 	public void callWhenReady() {
 
 		this.remotePlugCnx.safelyPassParameters(mFile, mSampleRate,
-				audioSource, outBitrate, postEncode,
-				"org.proofs.recorder.codec.mp3",
-				"org.proofs.recorder.codec.mp3.MainActivity", broadcastClass);
+				audioSource, outBitrate, postEncode, notificationIntent,
+				notificationPkg, broadcastClass);
 		this.remotePlugCnx.safelyStartRec();
 
 	}
---
+++ src/org/proof/recorder/utils/PlugMiddleware.java
@@ -0,0 +1,10 @@
+package org.proof.recorder.utils;
+
+public interface PlugMiddleware {
+	
+	void callWhenReady();
+	void parametersRecAsynchronously(int message);
+	void startRecAsynchronously(int message);
+	void stopRecAsynchronously(int message);
+
+}
