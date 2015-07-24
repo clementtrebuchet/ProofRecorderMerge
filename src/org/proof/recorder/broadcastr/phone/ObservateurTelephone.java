e258a9463adf8579ae4db7f10a6b6961de51ba91
Clément trebuchet <clement.trebuchet@gmail.com>
2015-07-24 23:36:55 +0200

    FIX : Typo

--- src/org/proof/recorder/broadcastr/phone/ObservateurTelephone.java
+++ src/org/proof/recorder/broadcastr/phone/ObservateurTelephone.java
@@ -16,257 +16,254 @@ import org.proof.recorder.utils.Log.Console;
 @SuppressWarnings("unused")
 class ObservateurTelephone extends PhoneStateListener {
 
-	public static String DIRECTION_CALL;
+    public static String DIRECTION_CALL;
 
-	private static String OUT_NUMBER = null;
+    private static String OUT_NUMBER = null;
 
-	private static String AUDIO_FORMAT = "3gp";
+    private static String AUDIO_FORMAT = "3gp";
 
-	// Stopping Service from excluded contacts list
-	// getting the SPEAKERON parameter to false by default for this case
+    // Stopping Service from excluded contacts list
+    // getting the SPEAKERON parameter to false by default for this case
 
-	private static boolean IS_EXCLUDED;
+    private static boolean IS_EXCLUDED;
 
-	private static Context _context = null;
+    private static Context _context = null;
 
-	private TelephonyManager _monManagerTel;
+    private TelephonyManager _monManagerTel;
 
-	private DataPersistanceManager dpm = null;
+    private DataPersistanceManager dpm = null;
 
-	private static Intent audioService;
+    private static Intent audioService;
 
-	/**
-	 * @return the IS_EXCLUDED
-	 */
-	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
-	public boolean isExcluded() {
-		return IS_EXCLUDED;
-	}
+    /**
+     * @return the IS_EXCLUDED
+     */
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    public boolean isExcluded() {
+        return IS_EXCLUDED;
+    }
 
-	/**
-	 */
-	private void setExcluded(boolean isExcluded) {
-		ObservateurTelephone.IS_EXCLUDED = isExcluded;
-	}
+    /**
+     */
+    private void setExcluded(boolean isExcluded) {
+        ObservateurTelephone.IS_EXCLUDED = isExcluded;
+    }
 
-	private void setContext(Context context) {
-		_context = context;
-		
-		if(!Settings.hasContext()) {
-			Settings.setSettingscontext(context);
-		}	
-	}
-	
-	private Context getContext() {
-		return _context;
-	}
+    private void setContext(Context context) {
+        _context = context;
 
-	/**
-	 * @return the AUDIO_FORMAT
-	 */
-	public static String getAudioFormat() {
-		return AUDIO_FORMAT;
-	}
+        if (!Settings.hasContext()) {
+            Settings.setSettingscontext(context);
+        }
+    }
 
-	/**
-	 */
-	public static void setAudioFormat(String _audioFormat) {
-		ObservateurTelephone.AUDIO_FORMAT = _audioFormat;
-	}
+    private Context getContext() {
+        return _context;
+    }
 
-	synchronized public void getManager(TelephonyManager telephony) {
-		_monManagerTel = telephony;
-	}
+    /**
+     * @return the AUDIO_FORMAT
+     */
+    public static String getAudioFormat() {
+        return AUDIO_FORMAT;
+    }
 
-	public void resetDpm() {	
-		dpm.cacheRows("CAN_RECORD", "0");
-	}
+    /**
+     */
+    public static void setAudioFormat(String _audioFormat) {
+        ObservateurTelephone.AUDIO_FORMAT = _audioFormat;
+    }
 
-	synchronized @Override
-	public void onCallStateChanged(int state, String incomingNumber) {	
+    synchronized public void getManager(TelephonyManager telephony) {
+        _monManagerTel = telephony;
+    }
 
-		if (getContext() == null) {
-			Console.print_exception("The Context is Null!");
-			return;	
-		}
-		
-		SetStaticContext.setStaticsContext(getContext(), 0);
-			
-		dpm = new DataPersistanceManager();
+    public void resetDpm() {
+        dpm.cacheRows("CAN_RECORD", "0");
+    }
 
-		switch (state) {
+    synchronized
+    @Override
+    public void onCallStateChanged(int state, String incomingNumber) {
 
-		case TelephonyManager.CALL_STATE_RINGING:			
+        if (getContext() == null) {
+            Console.print_exception("The Context is Null!");
+            return;
+        }
 
-			String info = "Le téléphone sonne " + incomingNumber;
+        SetStaticContext.setStaticsContext(getContext(), 0);
 
-			Console.print_debug(info);
+        dpm = new DataPersistanceManager();
 
-			if (Settings.isToastNotifications())
-				Toast.makeText(getContext(), info, Toast.LENGTH_SHORT).show();
+        switch (state) {
 
-			break;
-		case TelephonyManager.CALL_STATE_OFFHOOK:
+            case TelephonyManager.CALL_STATE_RINGING:
 
-			Contact.setResolver(
-					getContext().getApplicationContext().getContentResolver());
+                String info = "Le téléphone sonne " + incomingNumber;
 
-			boolean excluded;
-			Contact contact = new Contact();
+                Console.print_debug(info);
 
-			try {
-				contact.setPhoneNumber(OUT_NUMBER);
-			} catch (Exception e) {
-				Console.print_exception(e);
-			}			
+                if (Settings.isToastNotifications())
+                    Toast.makeText(getContext(), info, Toast.LENGTH_SHORT).show();
 
-			excluded = contact.isExcluded();
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
 
-			setExcluded(excluded);
+                Contact.setResolver(
+                        getContext().getApplicationContext().getContentResolver());
 
-			Console.print_debug("***********************************************");
-			Console.print_debug("Contact Number: " + OUT_NUMBER + " is excluded: " + excluded);
-			Console.print_debug("***********************************************");			
+                boolean excluded;
+                Contact contact = new Contact();
 
-			if (excluded) {
-				resetDpm();
-				return;
-			}
+                try {
+                    contact.setPhoneNumber(OUT_NUMBER);
+                } catch (Exception e) {
+                    Console.print_exception(e);
+                }
 
-			Console.print_debug("L'APPEL A ETE PRIS");
+                excluded = contact.isExcluded();
 
-			if(dpm.retrieveCachedRows("CAN_RECORD").equals("1")) {					
-				startRecording();
-			}
-			
-			break;
+                setExcluded(excluded);
 
-		case TelephonyManager.CALL_STATE_IDLE:
+                Console.print_debug("***********************************************");
+                Console.print_debug("Contact Number: " + OUT_NUMBER + " is excluded: " + excluded);
+                Console.print_debug("***********************************************");
 
-			Console.print_debug("L'APPEL Etat IDLE");	
-			
-			if(dpm.retrieveCachedRows("CAN_RECORD").equals("1")) {					
-				stopRecording();				
-			}									
+                if (excluded) {
+                    resetDpm();
+                    return;
+                }
 
-			_monManagerTel.listen(ObservateurTelephone.this,
-					PhoneStateListener.LISTEN_NONE);
-			break;
-		}
-		
-		Console.print_debug(incomingNumber);		
-		Console.print_debug("L'ETAT A CHANGER");
-	}
+                Console.print_debug("L'APPEL A ETE PRIS");
 
-	public void prepareRecording(String phoneNumber, String directionCall) {
-		
-		
-		// should fix when audio recorder is on 
-		// and an incoming / outgoing call is done at the same time.
-		// Avoiding strange behavior or/and bugs.
-		
-		// We let the service on, but we keep track of all data:
-		// Voice & Call, then we ask the user to choose what we're to do
-		// with this hybrid audio record.
-		
-		// ie. Save it as:
-		//     1 A voice record
-		//     2 A call record
-		//     3 both.
-		
-		// Note: we indicate the user that's not a recommended state :)
-		// Note: If option 1 or 3 is chosen, then we display Voice dialog for title edit purpose.
-		
-		audioService = new Intent(getContext(), PhoneRecorderReceiver.class);
-    	
-    	if(!dpm.isProcessing()) {
-    		// Normal behavior    		
-    		audioService.setAction("android.intent.action.START_PHONE_RECORDER");
-    	}    	
-    	else {
-    		audioService.setAction("android.intent.action.KEEP_PHONE_DATA_TRACK");
-    	}
-    	
-		audioService.putExtra("phoneNumber", phoneNumber);
-		audioService.putExtra("directionCall", directionCall);
-		
-		dpm.cacheRows("PhoneServicePrepared", "true");
-	}
-	
-	private void startRecording() {
-		
-		boolean isPrepared;
-		
-		try {
-			isPrepared = Boolean.parseBoolean(
-					dpm.retrieveCachedRows("PhoneServicePrepared"));
-		}
-		catch (Exception e) {
-			isPrepared = false;
-		}
-		
-		if(isPrepared) {
-		try {
-			getContext().sendBroadcast(audioService);			
-			
-			dpm.cacheRows("PhoneServicePrepared", "false");
-			dpm.cacheRows("PhoneServiceRunning", "true");
-		} catch (NullPointerExceptione){
-		    	Console.print_debug("Cant init recording avoiding crash: "+e.getMessage());
-		}
-		}	
-	}
+                if (dpm.retrieveCachedRows("CAN_RECORD").equals("1")) {
+                    startRecording();
+                }
+
+                break;
+
+            case TelephonyManager.CALL_STATE_IDLE:
+
+                Console.print_debug("L'APPEL Etat IDLE");
+
+                if (dpm.retrieveCachedRows("CAN_RECORD").equals("1")) {
+                    stopRecording();
+                }
+
+                _monManagerTel.listen(ObservateurTelephone.this,
+                        PhoneStateListener.LISTEN_NONE);
+                break;
+        }
+
+        Console.print_debug(incomingNumber);
+        Console.print_debug("L'ETAT A CHANGER");
+    }
+
+    public void prepareRecording(String phoneNumber, String directionCall) {
 
 
-	private void stopRecording() {
-		
-		boolean hasStarted;
-		
-		try {
-			hasStarted = Boolean.parseBoolean(
-					dpm.retrieveCachedRows("PhoneServiceRunning"));
-		}
-		catch (Exception e) {
-			hasStarted = false;
-		}
-		
-		if(hasStarted) {
-			Intent audioService = new Intent(getContext(), PhoneRecorderReceiver.class);
-			
-			if(!dpm.isProcessing()) {
-	    		// Normal behavior    		
-	    		audioService.setAction("android.intent.action.STOP_PHONE_RECORDER");
-	    	}    	
-	    	else {
-	    		audioService.setAction("android.intent.action.SAVE_PHONE_KEPT_DATA");
-	    	}
-			
-			getContext().sendBroadcast(audioService);  
-			
-			dpm.cacheRows("PhoneServiceRunning", "false");
-		}		
-	}	    
+        // should fix when audio recorder is on
+        // and an incoming / outgoing call is done at the same time.
+        // Avoiding strange behavior or/and bugs.
 
-	public ObservateurTelephone(Context context) {
-		super();
-		
-		Console.setTagName(this.getClass().getSimpleName());
-		
-		setContext(context);
-		
-		SetStaticContext.setStaticsContext(context, 0);
-		
-		dpm = new DataPersistanceManager();
-		
-		dpm.cacheRows("CAN_RECORD", "1");
-	}
+        // We let the service on, but we keep track of all data:
+        // Voice & Call, then we ask the user to choose what we're to do
+        // with this hybrid audio record.
 
-	public void feedNumbers(String phonenumber) {
-		OUT_NUMBER = phonenumber;
-	}
+        // ie. Save it as:
+        //     1 A voice record
+        //     2 A call record
+        //     3 both.
+
+        // Note: we indicate the user that's not a recommended state :)
+        // Note: If option 1 or 3 is chosen, then we display Voice dialog for title edit purpose.
+
+        audioService = new Intent(getContext(), PhoneRecorderReceiver.class);
+
+        if (!dpm.isProcessing()) {
+            // Normal behavior
+            audioService.setAction("android.intent.action.START_PHONE_RECORDER");
+        } else {
+            audioService.setAction("android.intent.action.KEEP_PHONE_DATA_TRACK");
+        }
+
+        audioService.putExtra("phoneNumber", phoneNumber);
+        audioService.putExtra("directionCall", directionCall);
+
+        dpm.cacheRows("PhoneServicePrepared", "true");
+    }
+
+    private void startRecording() {
+
+        boolean isPrepared;
+
+        try {
+            isPrepared = Boolean.parseBoolean(
+                    dpm.retrieveCachedRows("PhoneServicePrepared"));
+        } catch (Exception e) {
+            isPrepared = false;
+        }
+
+        if (isPrepared) {
+            try {
+                getContext().sendBroadcast(audioService);
+
+                dpm.cacheRows("PhoneServicePrepared", "false");
+                dpm.cacheRows("PhoneServiceRunning", "true");
+            } catch (NullPointerException e) {
+                Console.print_debug("Cant init recording avoiding crash: " + e.getMessage());
+            }
+        }
+    }
+
+
+    private void stopRecording() {
+
+        boolean hasStarted;
+
+        try {
+            hasStarted = Boolean.parseBoolean(
+                    dpm.retrieveCachedRows("PhoneServiceRunning"));
+        } catch (Exception e) {
+            hasStarted = false;
+        }
+
+        if (hasStarted) {
+            Intent audioService = new Intent(getContext(), PhoneRecorderReceiver.class);
+
+            if (!dpm.isProcessing()) {
+                // Normal behavior
+                audioService.setAction("android.intent.action.STOP_PHONE_RECORDER");
+            } else {
+                audioService.setAction("android.intent.action.SAVE_PHONE_KEPT_DATA");
+            }
+
+            getContext().sendBroadcast(audioService);
+
+            dpm.cacheRows("PhoneServiceRunning", "false");
+        }
+    }
+
+    public ObservateurTelephone(Context context) {
+        super();
+
+        Console.setTagName(this.getClass().getSimpleName());
+
+        setContext(context);
+
+        SetStaticContext.setStaticsContext(context, 0);
+
+        dpm = new DataPersistanceManager();
+
+        dpm.cacheRows("CAN_RECORD", "1");
+    }
+
+    public void feedNumbers(String phonenumber) {
+        OUT_NUMBER = phonenumber;
+    }
 
 	/*
-	 * true = ENTRANT false = SORTANT
+     * true = ENTRANT false = SORTANT
 	 */
 
 }
